cbe0ce867fe7a771daeb94dd6c0c9f569a22b3e9
Charles-Hugo Vialle <charly.vialle@gmail.com>
2021-07-21 16:28:50 +0200

    Accroche component

--- app/pages/index.js
+++ app/pages/index.js
@@ -2,12 +2,27 @@ import React from "react";
 import Accroche from "../sources/view/accroche/Accroche";
 import MyCity from "../sources/view/myCity/MyCity";
 import ListFriends from "../sources/view/listFriends/ListFriends";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    justifyContent: "center",
+    flexDirection: "column",
+    margin: "auto",
+  },
+  image : {
+    width: 600,
+  },
+})
 
 export default function Home() {
+  const classes = useStyles();
+
   return (
-    <div>
-      <img src="/assets/mapWorld.png" />
-      <Accroche />
+    <div className={classes.root}>
+      <img className={classes.image} src="/assets/mapWorld.png" />
+      <Accroche className={classes.accroche} />
       <MyCity />
       <ListFriends />
     </div>
--- app/sources/view/accroche/Accroche.jsx
+++ app/sources/view/accroche/Accroche.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles({
   root: {
-    minWidth: 275,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
+    width: 600,
   },
   title: {
     fontSize: 14,
+    display: "flex",
+    justifyContent: "center",
   },
-  pos: {
-    marginBottom: 12,
+  accroche: {
+    display: "flex",
+    justifyContent: "center",
   },
+  p: {
+    display: "flex",
+    justifyContent: "center",
+    textAlign: "center",
+  }
 });
 
 export default function Accroche() {
@@ -31,23 +32,15 @@ export default function Accroche() {
     <Card className={classes.root}>
       <CardContent>
         <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Word of the Day
+          World of the Day
         </Typography>
-        <Typography variant="h5" component="h2">
-          be{bull}nev{bull}o{bull}lent
+        <Typography className={classes.accroche} variant="h5" component="h2">
+          Travel{bull}Around{bull}the{bull}World{bull}and{bull}find{bull}your{bull}Friends
         </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography variant="body2" component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
+        <Typography className={classes.p} variant="body2" component="p">
+          Vous voyagez, visitez un pays, une ville, mais vous ne savez plus lequels de vos amis s'y trouvent. Recherchez, trouvez.
         </Typography>
       </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
     </Card>
   );
 }
